dae69d1fd06d0177adfd73de30655fa3c888387a
cacingsuper <cacingzuper@gmail.com>
2021-03-31 23:22:56 +0700

    first commit

---
+++ src/pages/About.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar';
+
+function About() {
+    return (
+        <View>
+            <StatusBar style="auto" />
+            <Text>About</Text>
+        </View>
+    )
+}
+
+export default About
---
+++ src/pages/Home.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar';
+
+function Home() {
+    return (
+        <View>
+            <StatusBar style="auto" />
+            <Text>Home</Text>
+        </View>
+    )
+}
+
+export default Home
